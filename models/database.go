cf60b2509ac998fb5731f79d74caf70fa89d67ee
Anggiprayoga28 <anggiprayoga930@gmail.com>
2025-11-10 08:42:33 +0700

    Fix: Move the database connection configuration file to the models folder.

---
+++ models/database.go
@@ -0,0 +1,49 @@
+package models
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/joho/godotenv"
+)
+
+var DB *pgxpool.Pool
+
+func InitDB() {
+	godotenv.Load()
+
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		getEnv("DB_HOST", "localhost"),
+		getEnv("DB_PORT", "5454"),
+		getEnv("DB_USER", "anggi"),
+		getEnv("DB_PASSWORD", ""),
+		getEnv("DB_NAME", "coffee_shop"))
+
+	var err error
+	DB, err = pgxpool.New(context.Background(), dsn)
+	if err != nil {
+		log.Fatal("DB connection failed:", err)
+	}
+
+	if err = DB.Ping(context.Background()); err != nil {
+		log.Fatal("DB ping failed:", err)
+	}
+
+	log.Println("Database connected")
+}
+
+func CloseDB() {
+	if DB != nil {
+		DB.Close()
+	}
+}
+
+func getEnv(key, def string) string {
+	if val := os.Getenv(key); val != "" {
+		return val
+	}
+	return def
+}
