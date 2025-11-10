7deb2b52b818b9024ee89d497dbd7514b3a2a314
Anggiprayoga28 <anggiprayoga930@gmail.com>
2025-11-10 22:01:23 +0700

    refactor: remove unused Category struct from product model

--- models/product.go
+++ models/product.go
@@ -2,13 +2,6 @@ package models
 
 import "time"
 
-type Category struct {
-	ID        int       `json:"id"`
-	Name      string    `json:"name"`
-	IsActive  bool      `json:"is_active"`
-	CreatedAt time.Time `json:"created_at"`
-}
-
 type Product struct {
 	ID          int       `json:"id"`
 	Name        string    `json:"name"`
