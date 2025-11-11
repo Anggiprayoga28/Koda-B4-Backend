c583af490b75efd77704472fe4b2bd04fbc2cc56
Anggiprayoga28 <anggiprayoga930@gmail.com>
2025-11-11 21:08:03 +0700

    Feat: Update Product struct to support promo features

--- models/product.go
+++ models/product.go
@@ -10,6 +10,9 @@ type Product struct {
 	Price       int       `json:"price"`
 	Stock       int       `json:"stock"`
 	ImageURL    string    `json:"image_url"`
+	IsFlashSale bool      `json:"is_flash_sale"`
+	IsFavorite  bool      `json:"is_favorite"`
+	IsBuy1Get1  bool      `json:"is_buy1get1"`
 	IsActive    bool      `json:"is_active"`
 	CreatedAt   time.Time `json:"created_at"`
 	UpdatedAt   time.Time `json:"updated_at"`
