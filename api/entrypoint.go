93dbc00756bad56a33c8a7343284e8c7b1df35c7
Anggiprayoga28 <anggiprayoga930@gmail.com>
2025-11-13 16:50:07 +0700

    Feat: Initialize server setup for Vercel deployment with DB, Redis, middleware, and routes

---
+++ api/entrypoint.go
@@ -0,0 +1,37 @@
+package api
+
+import (
+	"coffee-shop/middleware"
+	"coffee-shop/models"
+	"coffee-shop/routes"
+	"log"
+	"net/http"
+
+	_ "coffee-shop/docs"
+
+	"github.com/gin-gonic/gin"
+)
+
+var (
+	app *gin.Engine
+)
+
+func init() {
+	models.InitDB()
+
+	models.InitRedis()
+
+	gin.SetMode(gin.ReleaseMode)
+
+	app = gin.New()
+	app.Use(gin.Recovery())
+	app.Use(middleware.CORSMiddleware())
+
+	routes.SetupRoutes(app)
+
+	log.Println("Server initialized for Vercel")
+}
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	app.ServeHTTP(w, r)
+}
