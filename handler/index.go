d67ef505d805e1e4ea483c68b47b37554cd65b2f
Anggiprayoga28 <anggiprayoga930@gmail.com>
2025-11-13 21:38:42 +0700

    Fix: Minimal working handler

--- handler/index.go
+++ handler/index.go
@@ -1,19 +1,11 @@
 package handler
 
 import (
-	"encoding/json"
 	"net/http"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-
-	response := map[string]interface{}{
-		"status":  "ok",
-		"message": "Coffee Shop API",
-		"path":    r.URL.Path,
-	}
-
-	json.NewEncoder(w).Encode(response)
+	w.Write([]byte(`{"status":"ok","message":"API Working","path":"` + r.URL.Path + `"}`))
 }
