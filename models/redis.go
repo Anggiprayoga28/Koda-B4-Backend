f4126a8324f5c9c089c9e71ece0174d289a8c726
Anggiprayoga28 <anggiprayoga930@gmail.com>
2025-11-13 16:54:59 +0700

    Fix: Update Redis initialization to support REDIS_URL for production

--- models/redis.go
+++ models/redis.go
@@ -3,6 +3,7 @@ package models
 import (
 	"context"
 	"log"
+	"os"
 
 	"github.com/redis/go-redis/v9"
 )
@@ -10,16 +11,32 @@ import (
 var RedisClient *redis.Client
 
 func InitRedis() {
-	RedisClient = redis.NewClient(&redis.Options{
-		Addr:     getEnv("REDIS_ADDR", "localhost:6379"),
-		Password: getEnv("REDIS_PASSWORD", ""),
-		DB:       0,
-	})
+	redisURL := os.Getenv("REDIS_URL")
+
+	var opt *redis.Options
+	if redisURL != "" {
+		parsedOpt, err := redis.ParseURL(redisURL)
+		if err != nil {
+			log.Println("Failed to parse Redis URL:", err)
+			log.Println("Running without cache")
+			return
+		}
+		opt = parsedOpt
+	} else {
+		opt = &redis.Options{
+			Addr:     getEnv("REDIS_ADDR", "localhost:6379"),
+			Password: getEnv("REDIS_PASSWORD", ""),
+			DB:       0,
+		}
+	}
+
+	RedisClient = redis.NewClient(opt)
 
 	_, err := RedisClient.Ping(context.Background()).Result()
 	if err != nil {
 		log.Println("Redis connection failed:", err)
 		log.Println("Running without cache")
+		RedisClient = nil
 		return
 	}
 
