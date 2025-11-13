16bc00e517ec6abccdc2e1755e7cd18804eea908
Anggiprayoga28 <anggiprayoga930@gmail.com>
2025-11-13 11:18:27 +0700

    Feat: Add profile and history endpoints to API documentation

--- docs/docs.go
+++ docs/docs.go
@@ -1175,46 +1175,6 @@ const docTemplate = `{
                 }
             }
         },
-        "/history/{id}": {
-            "get": {
-                "security": [
-                    {
-                        "BearerAuth": []
-                    }
-                ],
-                "description": "Get detailed information about a specific order",
-                "produces": [
-                    "application/json"
-                ],
-                "tags": [
-                    "History"
-                ],
-                "summary": "Get order detail",
-                "parameters": [
-                    {
-                        "type": "integer",
-                        "description": "Order ID",
-                        "name": "id",
-                        "in": "path",
-                        "required": true
-                    }
-                ],
-                "responses": {
-                    "200": {
-                        "description": "OK",
-                        "schema": {
-                            "$ref": "#/definitions/models.Response"
-                        }
-                    },
-                    "404": {
-                        "description": "Not Found",
-                        "schema": {
-                            "$ref": "#/definitions/models.ErrorResponse"
-                        }
-                    }
-                }
-            }
-        },
         "/orders": {
             "post": {
                 "security": [
