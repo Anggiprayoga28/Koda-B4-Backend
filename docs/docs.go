cd49d93ed2d4be523c48c9be2794744cbf83aa42
Anggiprayoga28 <anggiprayoga930@gmail.com>
2025-11-16 22:30:57 +0700

    Fix: Changing the url

--- docs/docs.go
+++ docs/docs.go
@@ -1736,9 +1736,9 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8080",
+	Host:             "harlan-holden-coffee-backend.vercel.app",
 	BasePath:         "/",
-	Schemes:          []string{"http", "https"},
+	Schemes:          []string{"https"},
 	Title:            "Coffee Shop",
 	Description:      "Coffee Shop Management System API Documentation",
 	InfoInstanceName: "swagger",
