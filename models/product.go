5646fc58e6f2d4fab7604456200bd5b5962d2b2a
Anggiprayoga28 <anggiprayoga930@gmail.com>
2025-11-17 23:35:07 +0700

    Feat: Add CloudinaryID field to Product model

--- models/product.go
+++ models/product.go
@@ -3,17 +3,18 @@ package models
 import "time"
 
 type Product struct {
-	ID          int       `json:"id"`
-	Name        string    `json:"name"`
-	Description string    `json:"description"`
-	CategoryID  int       `json:"category_id"`
-	Price       int       `json:"price"`
-	Stock       int       `json:"stock"`
-	ImageURL    string    `json:"image_url"`
-	IsFlashSale bool      `json:"is_flash_sale"`
-	IsFavorite  bool      `json:"is_favorite"`
-	IsBuy1Get1  bool      `json:"is_buy1get1"`
-	IsActive    bool      `json:"is_active"`
-	CreatedAt   time.Time `json:"created_at"`
-	UpdatedAt   time.Time `json:"updated_at"`
+	ID           int       `json:"id"`
+	Name         string    `json:"name"`
+	Description  string    `json:"description"`
+	CategoryID   int       `json:"category_id"`
+	Price        int       `json:"price"`
+	Stock        int       `json:"stock"`
+	ImageURL     string    `json:"image_url"`
+	CloudinaryID string    `json:"cloudinary_id,omitempty"`
+	IsFlashSale  bool      `json:"is_flash_sale"`
+	IsFavorite   bool      `json:"is_favorite"`
+	IsBuy1Get1   bool      `json:"is_buy1get1"`
+	IsActive     bool      `json:"is_active"`
+	CreatedAt    time.Time `json:"created_at"`
+	UpdatedAt    time.Time `json:"updated_at"`
 }
