c57d166d6974c68dd1d4d41b2805f38e8f759cd5
Anggiprayoga28 <anggiprayoga930@gmail.com>
2025-11-20 13:40:45 +0700

    Feat: Add prefilght handle and multi origin

--- middleware/cors.go
+++ middleware/cors.go
@@ -1,19 +1,23 @@
 package middleware
 
 import (
-	"time"
+	"os"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func CORSMiddleware() gin.HandlerFunc {
+	originEnv := os.Getenv("ORIGIN_URL")
+
 	return cors.New(cors.Config{
-		AllowOrigins:     []string{"*"},
+		AllowOrigins: []string{
+			originEnv,
+			"http://localhost:5173",
+		},
 		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
 		AllowCredentials: true,
-		MaxAge:           12 * time.Hour,
 	})
 }
